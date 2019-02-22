0345dee93fc2454f5f04d8ed3fad58f5a666610f
user <user@s65.lab>
2019-02-22 14:37:35 +0900

    add multi experiment_xy result

--- uvfa_multiagent/check_buffer.py
+++ uvfa_multiagent/check_buffer.py
@@ -2,7 +2,6 @@ import numpy as np
 from matplotlib import pyplot
 episode = np.load('episode.npy')
 total_reward = np.load('total_reward.npy')
-latest_buffer = np.load('latest_buffer.npy')
 goal_list = np.load('goal_list.npy')
 print(goal_list)
 print(total_reward)
